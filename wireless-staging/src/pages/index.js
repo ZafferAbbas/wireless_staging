2f0006387ed1491d4778f519aa9ad9811f9420a3
Zaffer Abbas <zaffer62742@gmail.com>
2023-05-01 00:50:17 +0500

    fix

--- wireless-staging/src/pages/index.js
+++ wireless-staging/src/pages/index.js
@@ -1,5 +1,18 @@
 import * as React from "react"
 
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>test</h1>
+    </main>
+  )
+}
 
 export default IndexPage
 
